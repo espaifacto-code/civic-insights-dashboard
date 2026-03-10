ac065cac6b8a7d24c82c7423822205c77337a9d0
MikelLG <mikelguelbenzu@gmail.com>
2026-03-10 02:25:23 +0100

    added build

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/civic-insights-dashboard/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
